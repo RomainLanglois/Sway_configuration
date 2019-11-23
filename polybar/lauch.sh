dc5552f6f4279f3c931eaaa8c79d0c605dc7fee9
RomainLanglois <romain.langlois@viacesi.fr>
2019-11-23 12:08:32 +0100

    Update launch.sh: add log files

--- polybar/lauch.sh
+++ polybar/lauch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -r bar1 &
-polybar -r bar2 &
+echo "---" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
+polybar -r top >>/tmp/polybar_top.log 2>&1 &
+polybar -r bottom >>/tmp/polybar_bottom.log 2>&1 &
 
 echo "Bars launched..."
