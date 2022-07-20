5b7b5cf0bd4407e75755a77f7fde95cf21595403
RomainLanglois <rls_it@protonmail.com>
2022-07-20 20:50:32 +0200

    Removing functions and adding support for usbguard

--- scripts/usb_handler.sh
+++ scripts/usb_handler.sh
@@ -2,39 +2,39 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+NC='\033[0m' # No colors
 
 option=$1
 device=$2
 
-usage ()
-{
-	if [ -z "$1" ] || [ -z "$2" ];
+if [[ $option ]]
+then
+	if [[ $option == "mount" && $device ]];
 	then
-		/bin/echo -e "${RED} Bad parameters passed !"
-		/bin/echo -e "${RED} Usage: $0 <mount | umount> <device>"
-		/bin/echo -e "${RED} Example: $0 mount /dev/sdd1"
+		/usr/bin/sudo /bin/mkdir /media/device_$device && \
+		/usr/bin/sudo /bin/mount /dev/$device /media/device_$device && \
+		/bin/echo -e "${GREEN}[*] Device mounted ${NC}"
+	elif [[ $option == "umount" && $device ]];
+	then
+		/usr/bin/sudo /bin/sync && \
+		/usr/bin/sudo /bin/umount /media/device_$device && \
+		/usr/bin/sudo /bin/rmdir /media/device_$device && \
+		/bin/echo -e "${GREEN}[*] You can safely remove the device ${NC}"
+	elif [[ $option == "list" ]];
+	then
+		/usr/bin/sudo /usr/bin/usbguard list-devices
+	elif [[ $option == "allow" ]];
+	then
+		/usr/bin/sudo /usr/bin/usbguard allow-device $device
+	elif [[ $option == "block" ]];
+	then
+		/usr/bin/sudo /usr/bin/usbguard block-device $device
+	else
+		/bin/echo -e "${RED}[*] Wrong verb or parameter passed !"
+		/bin/echo -e "[*] Only <mount | umount | list | allow | block> options are available ${NC}"
 		exit 1
 	fi
-}
-
-mount_usb_key ()
-{
-	sudo /bin/mkdir /media/device_$1 && sudo /bin/mount /dev/$1 /media/device_$1 && /bin/echo -e "${GREEN} Device mounted"
-}
-
-umount_usb_key ()
-{
-	sudo /bin/sync && sudo /bin/umount /media/device_$1 && sudo /bin/rmdir /media/device_$1 && /bin/echo -e "${GREEN} Device demonted"
-}
-
-usage $option $device
-if [ $option == "mount" ];
-then
-	mount_usb_key $device
-elif [ $option == "umount" ];
-then
-	umount_usb_key $device
 else
-	/bin/echo -e "${RED} Only <mount | umount> options are available"
-	exit 1
+        /bin/echo -e "${RED}[*] Usage:"
+        /bin/echo -e "  $0 <verb> ${NC}"
 fi
