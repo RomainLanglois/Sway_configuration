b455d05f847dc251543a730a557356303f511a28
RomainLanglois <rls_it@protonmail.com>
2022-08-05 11:19:23 +0200

    Rename folder, improve brightness.sh script

---
+++ bin_scripts/brightness.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+brightness_file=/sys/class/backlight/intel_backlight/brightness
+brightness_value=$(/bin/cat /sys/class/backlight/intel_backlight/brightness)
+max_brightness=$(/bin/cat /sys/class/backlight/intel_backlight/max_brightness)
+
+if [[ $1 == "+" ]]
+then
+	brightness_value=$(($brightness_value + ($max_brightness*10/100)))
+	if [[ $brightness_value -ge $max_brightness ]]
+	then
+		/bin/echo $max_brightness | /usr/bin/sudo /usr/bin/tee $brightness_file
+	else
+		/bin/echo $brightness_value | /usr/bin/sudo /usr/bin/tee $brightness_file
+	fi
+elif [[ $1 == "-" ]]
+then
+	brightness_value=$(($brightness_value - ($max_brightness*10/100)))
+	/bin/echo $brightness_value | /usr/bin/sudo /usr/bin/tee $brightness_file
+else
+	/bin/echo $(($brightness_value*100/$max_brightness))'%'
+fi
