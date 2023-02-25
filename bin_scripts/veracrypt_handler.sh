e7135aab933eff9dae05e7dd2117654edad5604a
RomainLanglois <romain.langlois@protonmail.com>
2023-02-25 20:36:22 +0100

    Minor updates and add support for multiple containers

--- bin_scripts/veracrypt_handler.sh
+++ bin_scripts/veracrypt_handler.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# TODO
-# Handle multiple veracrypt container at the same time (SLOT number)
-# https://arcanecode.com/2021/06/21/veracrypt-on-the-command-line-for-ubuntu-linux/
-
 set -e
 
 RED='\e[31m'
@@ -12,39 +8,45 @@ YELLOW='\e[33m'
 NC='\e[0m' # No colors
 
 option=$1
-veracrypt_container=$2
+veracrypt_container_full_path=$2
+veracrypt_container=${2##*/}
+slot_number=1
 
-usage_function () {
-        /bin/echo -e "${RED}[*] Wrong option or parameter passed !"
-        /bin/echo -e "[*] Usage: "
+usage () {
+        /bin/echo -e "${RED}[*] Wrong option/parameter passed OR the specified container doesn't exists !${NC}"
+        /bin/echo -e "${YELLOW}[*] Usage: "
 	/bin/echo -e "	[*] $0 <option> <veracrypt_container>"
 	/bin/echo -e "	[*] Only <mount | umount | list> options are available ${NC}"
 	exit 1
 }
 
-
 if [[ $option ]]
 then
-	if [[ $option == "mount" && $veracrypt_container ]];
+	if [[ "$option" == "mount" && -f "$veracrypt_container_full_path" ]];
 	then
 		/usr/bin/sudo /bin/mkdir /media/$veracrypt_container && \
 		/usr/bin/sudo /bin/chown 1000:1000 /media/$veracrypt_container && \
 		/bin/echo -e "${YELLOW}[?] This veracrypt container is locked, please enter the password: ${NC}" && \
 		read -s password && \
-		/usr/bin/veracrypt --text --mount $veracrypt_container /media/$veracrypt_container --password $password --pim 0 --keyfiles "" --protect-hidden no --slot 1 && \
-		/bin/echo -e "${GREEN}[*] $veracrypt_container has been decrypted and mounted inside /media/$veracrypt_container/${NC}"
-	elif [[ $option == "umount" && $veracrypt_container ]];
+		until [[ "$slot_number" -ge 64 ]];
+		do
+			/usr/bin/veracrypt --text --mount $veracrypt_container_full_path /media/$veracrypt_container --password $password --pim 0 --keyfiles "" --protect-hidden no --hash sha512 --slot $slot_number 2> /dev/null && \
+			/bin/echo -e "${GREEN}[*] $veracrypt_container has been decrypted and mounted inside /media/$veracrypt_container/${NC}" && \
+			break
+			slot_number=$((slot_number+1))
+		done
+	elif [[ "$option" == "umount" && -d "$veracrypt_container_full_path" ]];
 	then
-		/usr/bin/veracrypt --text --dismount /media/$veracrypt_container && \
+		/usr/bin/veracrypt --text --dismount ${veracrypt_container_full_path%/} && \
 		/bin/sync && \
-		/usr/bin/sudo /bin/rmdir /media/$veracrypt_container && \
+		/usr/bin/sudo /bin/rmdir $veracrypt_container_full_path && \
 		/bin/echo -e "${GREEN}[*] $veracrypt_container container has been succesfully dismounted${NC}"
-	elif [[ $option == "list" ]];
+	elif [[ "$option" == "list" ]];
 	then
 		/usr/bin/sudo /usr/bin/veracrypt --text --list
 	else
-		usage_function
+		usage
 	fi
 else
-	usage_function
+	usage
 fi
