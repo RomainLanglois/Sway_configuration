840ea4b50ef5b562d76c9b6beda8087d858ff93c
RomainLanglois <romain.langlois@protonmail.com>
2023-02-28 21:33:49 +0100

    Major update

--- bin_scripts/convert_to_qcow2.sh
+++ bin_scripts/convert_to_qcow2.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No colors
+set -e
+
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+NC='\e[0m' # No colors
 
 usage ()
 {
 	/bin/echo -e "${RED}[*] Wrong parameter passed ! ${NC}"
-	/bin/echo "Usage :"
-	/bin/echo "	$0 <virtual_disk>"
-	/bin/echo "Example: "
-	/bin/echo "	$0 kali_linux.ova"
-	/bin/echo "	$0 kali_linux.vmdk"
-	/bin/echo "	$0 kali_linux.vdi"
+	/bin/echo -e "${YELLOW}Usage :"
+	/bin/echo -e "	$0 <virtual_disk>"
+	/bin/echo -e "Example: "
+	/bin/echo -e "	$0 ./kali_linux.ova"
+	/bin/echo -e "	$0 /var/lib/kali_linux.vmdk"
+	/bin/echo -e "	$0 /opt/kali_linux.vdi${NC}"
 	exit 1
 }
 
-virtual_disk=$1
-virtual_disk_extension=$(/bin/echo "${virtual_disk##*.}")
-virtual_disk_no_extension=$(/usr/bin/basename $virtual_disk .$virtual_disk_extension)
+virtual_disk=$(/usr/bin/basename "$1")
+virtual_disk_extension=${1##*.}
+virtual_disk_path=$(/usr/bin/dirname "$1")
+virtual_disk_without_extension=$(/usr/bin/basename -s ".$virtual_disk_extension" "$virtual_disk")
 
-if [[ $virtual_disk && $virtual_disk_extension == "ova" ]]
+if [[ $virtual_disk && $virtual_disk_extension == "ova" ]];
 then
-	/bin/echo "[*] ova file format detected !"
-	/bin/echo "[*] Extracting VMDK file from OVA..." && \
-	/bin/tar xvf $virtual_disk && \
-	/bin/echo "[*] Converting VMDK file to QCOW2" && \
-	vmdk_file=$(/bin/ls *.vmdk) && \
-	/usr/bin/qemu-img convert -O qcow2 $vmdk_file $virtual_disk_no_extension.qcow2 && \
-	/bin/echo "[*] Cleaning current directory" && \
-	/bin/rm $virtual_disk_no_extension.ovf $virtual_disk_no_extension.mf $vmdk_file && \
+	/bin/echo -e "${YELLOW}[!] ova file format detected !"
+	/bin/echo -e "[*] Extracting VMDK file from OVA...${NC}" && \
+	/bin/tar -xvf "$virtual_disk_path/$virtual_disk" && \
+	/bin/echo -e "${YELLOW}[*] Converting VMDK file to QCOW2${NC}" && \
+	vmdk_file=$(/usr/bin/find "$virtual_disk_path" -name "*.vmdk") && \
+	/usr/bin/qemu-img convert -O qcow2 "$vmdk_file" "${HOME}/KVM_images/$virtual_disk_without_extension.qcow2" && \
+	/bin/echo -e "${YELLOW}[*] Cleaning current directory${NC}" && \
+	/bin/rm *.ovf *.mf "$vmdk_file" && \
 	/bin/echo -e "${GREEN}[*] Done ! ${NC}"
-elif [[ $virtual_disk && $virtual_disk_extension == "vmdk" || $virtual_disk && $virtual_disk_extension == "vdi" ]]
+elif [[ $virtual_disk && $virtual_disk_extension == "vmdk" || $virtual_disk && $virtual_disk_extension == "vdi" ]];
 then
-	
-	/bin/echo "[*] $virtual_disk_extension file format detected !"
-	/bin/echo "[*] Converting $virtual_disk_extension file to QCOW2..." && \
-	/usr/bin/qemu-img convert -O qcow2 $virtual_disk $virtual_disk_no_extension.qcow2 && \
+	/bin/echo -e "${YELLOW}[!] "$virtual_disk_extension" file format detected !"
+	/bin/echo -e "[*] Converting "$virtual_disk_extension" file to QCOW2...${NC}" && \
+	/usr/bin/qemu-img convert -O qcow2 "$virtual_disk_path/$virtual_disk" "${HOME}/KVM_images/$virtual_disk_without_extension.qcow2" && \
 	/bin/echo -e "${GREEN}[*] Done ! ${NC}"
 else
 	usage
